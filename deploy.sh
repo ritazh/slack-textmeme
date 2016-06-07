e87b3f41f0b06215dda1a1bd09675593bac2d732
Rita Zhang <rita.z.zhang@gmail.com>
2016-06-07 13:51:00 -0700

    fix(CI) - Add travis and auto deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+git init
+git config user.name "Travis CI"
+git config user.email "ritazh@microsoft.com"
+
+echo 'Adding files to local repo'
+ls -ltr
+git add .
+git commit -m "Deploy"
+openssl aes-256-cbc -d -k password -in deis.enc -out deis.key -a
+cat ./deis.key
+
+# deis login http://deis.deisdemo.40.78.99.3.xip.io
+# send "deis\r"
+# send "deis\r"
+# deis apps
+
+GIT_TARGET_URL="ssh://git@deis.deisdemo.40.78.99.3.xip.io:2222/zydeco-gatepost.git "
+
+eval "$(ssh-agent -s)"
+ssh-agent -s
+
+chmod 600 ./deis.key
+ssh-add ./deis.key
+
+echo 'Private keys added. Starting Dokku Deployment'
+git remote add deis $GIT_TARGET_URL
+
+git push -f deis master
+
+echo 'Deployed Latest Version to Deis'
