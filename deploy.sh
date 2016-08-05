91a8612d5f4148849cdf645ed1f134007f89a4df
Rita Zhang <rita.z.zhang@gmail.com>
2016-08-04 23:43:01 -0700

    🚢  CI Deploy to Deis

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ openssl aes-256-cbc -d -k password -in deis.enc -out deis.key -a
 
 # write the trusted host to the known hosts file
 # to avoid a prompt when connecting to it via ssh
-DEIS_HOST="[deis.deisdemo.40.78.99.3.xip.io]:2222,[40.78.99.3]:2222"
+DEIS_HOST="[deis.deisdemo.40.84.5.106.xip.io]:2222,[40.84.5.106]:2222"
 echo $DEIS_HOST > ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
 
-GIT_TARGET_URL="ssh://git@deis.deisdemo.40.78.99.3.xip.io:2222/zydeco-gatepost.git "
+GIT_TARGET_URL="ssh://git@deis.deisdemo.40.84.5.106.xip.io:2222/zipped-whitecap.git "
 
 eval "$(ssh-agent -s)"
 ssh-agent -s
@@ -24,7 +24,7 @@ chmod 600 ./deis.key
 ssh-add ./deis.key
 
 echo 'Private keys added. Starting Deis Deployment'
-echo -e "Host deis.deisdemo.40.78.99.3.xip.io\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host deis.deisdemo.40.84.5.106.xip.io\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git remote add deis $GIT_TARGET_URL
 
 git push -f deis master
