44e3bfe20fa913204b11de5ae03565504aa1869d
Rita Zhang <rita.z.zhang@gmail.com>
2016-08-07 21:02:43 -0700

    :package: Travis deploy to Deis

--- deploy.sh
+++ deploy.sh
@@ -27,6 +27,6 @@ echo 'Private keys added. Starting Deis Deployment'
 echo -e "Host deis.deisdemo.40.84.5.106.xip.io\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git remote add deis $GIT_TARGET_URL
 
-git push -f deis master
+git push -f deis botframeworkdeis
 
 echo 'Deployed Latest Version to Deis'
