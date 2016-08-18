cea6dd1ff04e649738bea8f856033149ae1a6dd1
Rita Zhang <rita.z.zhang@gmail.com>
2016-08-17 23:48:46 -0700

    🔨 Update Travis CI to include deis pull from docker hub

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -27,6 +27,8 @@ echo 'Private keys added. Starting Deis Deployment'
 echo -e "Host deis.deisdemo.40.84.5.106.xip.io\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git remote add deis $GIT_TARGET_URL
 
-deis pull ritazh/textmemebot
+curl -sSL http://deis.io/deis-cli/install.sh | sh -s 1.12.2
+./deis login http://deis.deisdemo.40.84.5.106.xip.io --username=deis --password=deis
+./deis pull ritazh/textmemebot
 
 echo 'Deployed Latest Version to Deis'
